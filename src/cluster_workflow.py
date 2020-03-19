87e4db134f4a90869eb77465b45f974051bcf69d
John McCloskey <john.gaoxing@gmail.com>
2020-03-19 14:06:33 -0500

    Multiple (see description)
    - add logtransform, plot_cluster and plot_radar flags and functions (radar function does not work yet)
    - add save params and save code to all graph functions
    - fix bug in correlations where graph is built on previous graph
    - change PCA names to PCA_0 to n-1 instead of PCA_1 to n (for consistency)
    - change labels from df to nparray (merge with np_labels)
    - return PCA df as PCA only
    - in RunWorkflow - 4 dfs: original, working (just called df), PCA, cluster. That way, we don't have to track what is in each df and can send in different ones to different graphs
    - changes to main() for testing

--- src/cluster_workflow.py
+++ src/cluster_workflow.py
@@ -31,6 +31,8 @@ class Workflow:
     DEFAULT_SCALE = "robust"
 
     def __init__(self, filter_options, base_output_dir=None, nested_folder_output=True):
+        self.plot_cluster_scatter = True
+        self.plot_radars = True
         utils.init_path()
         self.filter_options = filter_options
         self._nested_folder_output = nested_folder_output
@@ -41,9 +43,9 @@ class Workflow:
 
         # steps
         self.pre_histogram = True
-        self.do_logtransform = None
+        self.do_logtransform = True
         self.do_scaling = True
-        self.do_normalization = True
+        self.do_normalization = False
         self.post_histogram = True
         self.plot_correlation = True
         self.do_PCA = True
@@ -89,7 +91,7 @@ class Workflow:
     def get_filename(self):
         return None  # some_string
 
-    def Histogram(self, df: pd.DataFrame, num_bins: int = None, title: str = None, log_scale=True):
+    def Histogram(self, df: pd.DataFrame, num_bins: int = None, title: str = None, log_scale=True, save=True):
         title = title or 'Histograms'
         num_rows = len(df.index)
         num_bins = num_bins or min(25, num_rows)
@@ -101,8 +103,13 @@ class Workflow:
         #     for ax in axrow:
         #         print(ax)
         #         ax.set_yscale('log')
+        if save:
+            savepath = os.path.join(self.get_base_output_dir(), f'{title}.png')
+            plt.savefig(savepath)
 
-    def Correlations(self, df, heat_range=0.3):
+    # TODO: Graph is part cut off, i think there might be some stuff hardcoded.
+    def Correlations(self, df, heat_range=0.3, save=True):
+        plt.figure()
         seaborn.set(style="ticks")
         corr = df.corr()
         g = seaborn.heatmap(corr, vmax=heat_range, center=0,
@@ -110,9 +117,15 @@ class Workflow:
         seaborn.despine()
         g.figure.set_size_inches(14, 10)
 
+        title = 'Correlations'
+        if save:
+            savepath = os.path.join(self.get_base_output_dir(), f'{title}.png')
+            g.figure.savefig(savepath)
 
     def LogTransformed(self, df):
-        return df, []
+        nparray = df.to_numpy()
+        nparray = np.log1p(nparray)
+        return pd.DataFrame(nparray, columns=df.columns), []
         pass
 
     # @df_np_df
@@ -135,19 +148,24 @@ class Workflow:
         nparray = df.to_numpy()
         dimension_count = dimension_count or self.pca_dimension_count
         nparray = PCA(n_components=dimension_count).fit_transform(nparray)
-        PCA_names = [f"PCA_{i + 1}" for i in range(dimension_count)]
+        PCA_names = [f"PCA_{i}" for i in range(dimension_count)]
         return pd.DataFrame(nparray, columns=PCA_names), []
 
-    def Scree(self, df):
+    def Scree(self, df, save=True):
         nparray = df.to_numpy()
         U, S, V = np.linalg.svd(nparray)
         eigvals = S ** 2 / np.sum(S ** 2)
         fig = plt.figure(figsize=(8, 5))
         singular_vals = np.arange(nparray.shape[1]) + 1
         plt.plot(singular_vals, eigvals, 'ro-', linewidth=2)
-        plt.title('Scree Plot')
+        title = 'Scree Plot'
+        plt.title(title)
         plt.xlabel('Principal Component')
         plt.ylabel('Eigenvalue')
+
+        if save:
+            savepath = os.path.join(self.get_base_output_dir(), f'{title}.png')
+            plt.savefig(savepath)
         return
 
     def Cluster(self, df, cluster_count: int, clustering_method=None):
@@ -162,33 +180,32 @@ class Workflow:
             # In the future, this will let us find centers:
             # distances = clusterer.transform(nparray)
             # nparray = np.concatenate((distances, labels))
-            nparray = labels
         # elif clustering_method == "FuzzyCMeans":
         #     pass
         elif clustering_method == "DBSCAN":
             labels = DBSCAN(eps=0.3, min_samples=10).fit_predict(nparray)
-            nparray = labels
-        return pd.DataFrame(nparray, columns=["labels"]), []
+        else:
+            labels = []
+        return labels,[]
 
         # for a,l in zip(PCA_dims, labels):
         #     b =  clustering.cluster_centers_[l]
         #     distances.append(a-b)
-        # df['label'] = labels
+        # labels = labels
         # df['PCA1 Offset'] = np.array(distances)[:,0]
         # df['PCA2 Offset'] = np.array(distances)[:,1]
 
-    def Silhouettes(self, dimension_data: pd.DataFrame, labels: pd.DataFrame, title=None):
+    def Silhouettes(self, dimension_data: pd.DataFrame, labels: pd.DataFrame, title=None, save=True):
         np_dimensions = dimension_data.to_numpy()
-        np_labels = labels.to_numpy().flatten()
-        silhouette_vals = silhouette_samples(np_dimensions, np_labels)
+        silhouette_vals = silhouette_samples(np_dimensions, labels)
 
         # Silhouette plot
         fig, ax1 = plt.subplots(1, 1)
         fig.set_size_inches(18, 7)
         y_ticks = []
         y_lower, y_upper = 0, 0
-        for i, cluster in enumerate(np.unique(np_labels)):
-            cluster_silhouette_vals = silhouette_vals[np_labels == cluster]
+        for i, cluster in enumerate(np.unique(labels)):
+            cluster_silhouette_vals = silhouette_vals[labels == cluster]
             cluster_silhouette_vals.sort()
             y_upper += len(cluster_silhouette_vals)
             ax1.barh(range(y_lower, y_upper), cluster_silhouette_vals, edgecolor='none', height=1)
@@ -202,23 +219,100 @@ class Workflow:
         ax1.set_xlim([-0.1, 1])
         ax1.set_xlabel('Silhouette coefficient values')
         ax1.set_ylabel('Cluster labels')
-        title = title or 'Silhouette plot for the various clusters'
+        title = title or f'Silhouette Plot Avg={int(avg_score*100)}%'
         ax1.set_title(title, y=1.02)
+        if save:
+            savepath = os.path.join(self.get_base_output_dir(), f'{title}.png')
+            plt.savefig(savepath)
 
         return
+
+
+    def scatter(self, df, labels, save=True):
+        num_cols = len(df.columns)
+        color_array = [self.color_dict[c] for c in labels]
+        fig, axs = plt.subplots(num_cols, num_cols, figsize=(30, 30))
+        for x in range(num_cols):
+            for y in range(num_cols):
+                axs[x, y].scatter(df.iloc[:, x], df.iloc[:, y], c=color_array)
+                axs[x, y].set_xlabel(df.columns[x])
+                axs[x, y].set_ylabel(df.columns[y])
+        title = 'Scatter'
+        if save:
+            savepath = os.path.join(self.get_base_output_dir(), f'{title}.png')
+            plt.savefig(savepath)
+
+    def radarCharts(self, df, labels, save=True):
+        clusters = set(labels)
+        categories = self.filter_options.finalfeats_readable
+        description_df = df.describe()
+        summary_df = pd.DataFrame(columns=description_df.columns)
+        clusters = set(labels)
+        cluster_dict = {}
+        for c in clusters:
+            cluster_dict[c] = df[labels == c]
+            cluster_df = cluster_dict[c].describe()
+            summary_df.loc[f'C{c}_zscore', :] = (cluster_df.loc['mean', :] - description_df.loc['mean', :]) / description_df.loc[
+                                                                                                       'std', :]
+            summary_df.loc[f'C{c}_%mean', :] = (cluster_df.loc['mean', :] / description_df.loc['mean', :]) * 100
+            summary_df.loc[f'C{c}_%std', :] = (cluster_df.loc['std', :] / description_df.loc['std', :]) * 100
+        summary_df = summary_df.apply(lambda x: (x * 100) // 1 * .01)
+
+        def make_spider(color, i):
+            offset = .25 * pi
+            # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+            angles = [n / float(N) * 2 * pi + offset for n in range(N)]
+            angles += angles[:1]
+            ax = plt.subplot(nrows, ncols, i + 1, polar=True)
+            plt.xticks(angles[:-1], categories, color='grey', size=12)
+            ax.set_rlabel_position(0)
+            if var == 'zscore':
+                plt.yticks([-2, -1, 0, 1, 2], color="grey", size=7)
+                plt.ylim(-2, 2)
+            elif '%' in var:
+                plt.yticks(range(0, 1000, 100), color="grey", size=7)
+                plt.ylim(0, 400)
+            values = list(tdf.iloc[i, :-3])
+            values += values[:1]
+            graph_name = tdf.index[i]
+            print(angles, values)
+            ax.plot(angles, values, color=color, linewidth=2, linestyle='solid')
+            ax.fill(angles, values, color=color, alpha=0.4)
+            plt.title(graph_name + f' (n={len(cluster_dict[i])})', size=11, color=color, y=1.1)
+
+        # number of variable
+        for var in ['zscore', '%mean', '%std']:
+            tdf = summary_df.loc[[idx for idx in summary_df.index if var in idx], :]
+            if not categories:
+                categories = list(tdf.columns)
+            N = len(categories)
+            num_groups = len(tdf.index)
+            #   nrows = 2
+            #   ncols = num_groups//2 if not num_groups%2 else num_groups//2 + 1
+            nrows = 1
+            ncols = num_groups
+            fig = plt.figure(figsize=(20, 5))
+            fig.suptitle(f'{var} Radar Charts')
+            for i in range(num_groups):
+                make_spider(self.color_dict[i], i)
+            fig.subplots_adjust(wspace=0.4)
+            if save:
+                plt.savefig(f'{self.get_cluster_output_dir()}/radar_{var}.png')
+        pass
 
     def SaveMeta(self, meta_list):
         print("SaveMeta: Stubbed function!")
         return None, []
 
     def RunWorkflow(self, get_df_func):
-        df, meta = cu.full_filter(get_df_func, self.filter_options)
-
+        original_df, meta = cu.full_filter(get_df_func, self.filter_options)
+        df = original_df.copy()
+        original_cols = list(df.columns)
         # Preprocessing - LogTransform, Scaling, Normalization #
         # show df before any processing
 
         if self.pre_histogram:
-            self.Histogram(df, title='Pre Normalization Histogram')
+            self.Histogram(df, title='Raw Histogram')
         # do log transform
         if self.do_logtransform:
             df, md = self.LogTransformed(df)
@@ -233,7 +327,7 @@ class Workflow:
             meta.extend(md)
         # show df after transformation
         if self.post_histogram:
-            self.Histogram(df, title='Post Transformation Histogram')
+            self.Histogram(df, title='Preprocessed Histogram')
 
         # correlation
         if self.plot_correlation:
@@ -243,18 +337,28 @@ class Workflow:
         if self.plot_scree:
             self.Scree(df)
         if self.do_PCA:
-            df, md = self.PCA(df)
+            pca_df, md = self.PCA(df)
             meta.extend(md)
+            cluster_df = pca_df
+        else:
+            cluster_df = df
 
         # silhouette and clustering
         if self.do_clustering:
-            column_names = [f"PCA{i}" for i in range(self.pca_dimension_count)]
-            labels, md = self.Cluster(df[column_names], cluster_count=self.clustering_count)
+            labels, md = self.Cluster(cluster_df, cluster_count=self.clustering_count)
             meta.extend(md)
-        if self.plot_silhouettes:
-            self.Silhouettes(df, labels)
+
+            if self.plot_silhouettes:
+                self.Silhouettes(cluster_df, labels)
+            if self.plot_cluster_scatter:
+                self.scatter(df, labels)
+
+            if self.plot_radars:
+                self.radarCharts(original_df, labels)
 
         return df, meta
+
+
 
 
 def add_cluster_features_to_df(pipeline, df, data):
@@ -266,17 +370,21 @@ def add_cluster_features_to_df(pipeline, df, data):
     for a, l in zip(PCA_dims, labels):
         b = clustering.cluster_centers_[l]
         distances.append(a - b)
-    df['label'] = labels
+    labels = labels
     df['PCA1 Offset'] = np.array(distances)[:, 0]
     df['PCA2 Offset'] = np.array(distances)[:, 1]
 
 
 def main():
     utils.init_path()
-    test_on_1125_data()
+    # test_on_1125_data()
+    w = Workflow(cu.options.lakeland_achs_achs_per_sess_second_sessDur, r'G:\My Drive\Field Day\Research and Writing Projects\2020 CHI Play - Lakeland Clustering\Jupyter\Results\Lakeland\test')
+    w.plot_silhouettes, w.plot_cluster_scatter = False,False
+    w.RunWorkflow(cu.getDecJanLogDF)
 
 
 def test_on_1125_data():
+
     base_dir = r'Results/Lakeland/1125_Clusters'
     for options, pca_dims, k in [(cu.options.lakeland_feedback_lv01, 3, 7),
                                  (cu.options.lakeland_player_lvl01, 3, 6),
